96775ffb660f3576c16647618265a774f7105139
sorididim11 <sorididim11@gmail.com>
2019-05-09 19:54:06 +0900

    refactor dirs

--- customized/terraform/azure/vars.tf
+++ customized/terraform/azure/vars.tf
@@ -36,7 +36,7 @@ variable "api_loadbalancer_backend_port" {
 variable "num_slaves" { default = "1" }
 variable "num_masters" { default = "1" }
 
-variable "ansible_inventory_home" { default = "../../inventory/azure" }
+variable "ansible_inventory_home" { default = "../../../inventory/azure" }
 
 
 variable "location" {
